6a5fbe49c5a9d150f4f5a3d9daf8af73837f4f43
ellie32411 <ellie32411@gmail.com>
2023-02-02 01:50:06 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const Record = require('./models/record')
+const routes = require('./routes')
+
 const app = express()
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -27,56 +29,7 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  Record.find()
-    .lean()
-    .sort({ _id: 'asc' })
-    .then(records => res.render('index', { records }))
-    .catch(error => console.log(error))
-})
-
-app.get('/records/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/records', (req, res) => {
-  const { name, amount } = req.body
-  return Record.create({ 
-    name,
-    amount
-   })
-   .then(() => res.redirect('/'))
-   .catch(error => console.log(error))
-})
-
-app.get('/records/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Record.findById(id)
-    .lean()
-    .then((record) => res.render('edit', { record }))
-    .catch((error) => console.log(error))
-})
-
-app.put('/records/:id', (req, res) => {
-  const id = req.params.id
-  const { name, amount } = req.body
-  return Record.findById(id)
-    .then((record) => {
-      record.name = name
-      record.amount = amount
-      return record.save()
-    })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-app.delete('/records/:id', (req, res) => {
-  const id = req.params.id
-  return Record.findById(id)
-    .then((record) => record.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+app.use(routes)
 
 // 設定 port 3000
 app.listen(3000, () => {
