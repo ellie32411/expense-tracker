cae6a4e66783db14a515e321527c7d8204976675
ellie32411 <ellie32411@gmail.com>
2023-02-02 16:14:56 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
 const Record = require('./models/record')
 const routes = require('./routes')
-
+require('./config/mongoose')
 const app = express()
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,15 +1,10 @@
-const mongoose = require('mongoose')
 const Record = require('../record') // 載入 todo model
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
+const db = require('../../config/mongoose')
 
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', () => {
   console.log('mongodb connected!')
   for (let i = 0; i < 10; i++) {
