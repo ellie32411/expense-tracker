4960fbeab2e524c2b12cd37b410deedf25012322
ellie32411 <ellie32411@gmail.com>
2023-02-07 16:11:48 +0800

    feat: modify home route

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
-const dayjs = require('dayjs')
 
 router.get('/', (req, res) => {
   const userId = req.user._id
   Record.find({ userId })
     .lean()
     .sort({ _id: 'asc' })
-    .then(records => res.render('index', { records }))
+    .then(records => {
+      let totalAmount = 0
+      records.forEach(record => {
+        totalAmount += record.amount
+      })
+      res.render('index', {records, totalAmount})
+    })
     .catch(error => console.log(error))
 })
 
